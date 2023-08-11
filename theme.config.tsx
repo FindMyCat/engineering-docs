2bb1f0fc167a0165d869fbeac4d8637e1694087e
Chitlange Sahas <chitlangesahas@gmail.com>
2023-08-10 20:02:07 -0700

    chore: theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>FindMyCat - Open Source Pet Tracker</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/FindMyCat/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'FindMyCat by ChitlangeSahas',
   },
 }
 
