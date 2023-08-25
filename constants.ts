a78bfda957256fea610d690d2959ee6d83a57099
Chitlange Sahas <chitlangesahas@gmail.com>
2023-08-25 16:05:43 -0700

    feat: slack invite in constant

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const SLACK_INVITE_LINK = "https://join.slack.com/t/findmycat/shared_invite/zt-21wo1f57m-zVcXlUkAvFmwDA3k5uYysw";
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import ImageProvider from "./pages/ImageProvider";
 import slack from "./public/slack.png"
 import Image from "next/image";
+import {SLACK_INVITE_LINK} from "./constants";
 
 const config: DocsThemeConfig = {
   logo: <span>FindMyCat - Open Source Pet Tracker</span>,
@@ -22,7 +22,7 @@ const config: DocsThemeConfig = {
     defaultMenuCollapseLevel: 1
   },
   chat: {
-    link: "https://join.slack.com/t/findmycat/shared_invite/zt-21wo1f57m-zVcXlUkAvFmwDA3k5uYysw",
+    link: SLACK_INVITE_LINK,
     icon: <Image src={slack} width={25} alt={"slack"}/>
   }
 }
