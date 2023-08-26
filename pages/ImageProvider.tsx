42f1a381caedbcb13a34afd0577e59710d216dbc
Chitlange Sahas <chitlangesahas@gmail.com>
2023-08-26 07:10:47 -0700

    feat: engineering render added

--- pages/ImageProvider.tsx
+++ pages/ImageProvider.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 interface OwnProps {
     lightImage: any
     darkImage: any
+    width?: number
 }
 export const ImageProvider = (props: OwnProps) => {
     let {theme, systemTheme} = useTheme()
@@ -10,9 +11,9 @@ export const ImageProvider = (props: OwnProps) => {
         theme = systemTheme;
     }
     if (theme === 'dark') {
-        return <div><Image src={props.darkImage} alt={"Dark Image"}/></div>
+        return <div><Image src={props.darkImage} alt={"Dark Image"} {...props}/></div>
     } else {
-        return <div><Image src={props.lightImage} alt={"LightImage"}/></div>
+        return <div><Image src={props.lightImage} alt={"LightImage"} {...props}/></div>
     }
 }
 
